3f32c405d5a36faf5af9e5c4c74e6bfff46a8228
Coriolando <coriolando.medeiros@gmail.com>
2024-08-25 14:59:10 -0300

    feat: adicionado cálculo de IMC

--- calorias.rb
+++ calorias.rb
@@ -57,8 +57,45 @@ def menu
 
 end
 
+def calculo_imc
+  @peso_imc = @peso
+  @altura_imc = @altura / 100
+  @imc = @peso_imc / (@altura_imc * @altura_imc)
 
-def calculo
+  if @imc < 16.9
+    puts "#{@nome}, seu IMC é #{'%.2f' % @imc}, magreza extrema!"
+    puts "Você deve ganhar peso para ser saudável"
+  elsif (@imc >= 17) && (@imc < 18.5)
+    puts "#{@nome}, seu IMC é #{'%.2f' % @imc}, abaixo do peso"
+    peso_ideal_min = 18.5 * (@altura_imc * @altura_imc)
+    peso_necessario = peso_ideal_min - @peso_imc
+    puts "Você deve ganhar #{'%.2f' % peso_necessario} kg  para ser saudável"
+  elsif (@imc >= 18.5) && (@imc < 25)
+    puts "#{@nome}, seu IMC é #{'%.2f' % @imc}, peso normal"
+  elsif (@imc  >= 25) && (@imc < 30)
+    puts "Seu IMC é #{@imc}, acima do peso"
+    peso_ideal_max = 24.9 * (@altura_imc * @altura_imc)
+    peso_necessario = @peso_imc - @peso_ideal_max
+    puts "Você deve perder #{'%.2f' % peso_necessario} kg  para ser saudável"
+  elsif (@imc >= 30) && (@imc < 35)
+    puts "Seu IMC é #{@imc}, obesidade grau I"
+    peso_ideal_max = 24.9 * (@altura_imc * @altura_imc)
+    peso_necessario = @peso_imc - @peso_ideal_max
+    puts "Você deve perder #{'%.2f' % peso_necessario} kg  para ser saudável"
+  elsif (@icm >= 35) && (@imc - 40)
+    puts "Seu IMC é #{'%.2f' % @imc}, obesidade grau II - Severa"
+    peso_ideal_max = 24.9 *(@altura_imc * @altura_imc)
+    peso_necessario = @peso_imc - peso_ideal_max
+    puts "Você deve perder #{'%.2f' % peso_necessario} kg para ser saudável"
+  elsif @imc >= 40
+    puts "Seu IMC é #{'%.2f' % @imc}, obesidade grau III - mórbida"
+    peso_ideal_max = 24.9 * (@altura_imc * @altura_imc)
+    peso_necessario = @peso_imc - peso_ideal_max
+    puts "Você deve perder #{'%.2f' % peso_necessario} kg para ser saudável"
+  end
+end
+
+def calculo_metabolismo
   metabolismo_basal = 88.362 + (13.397 * @peso) + (4.799 * @altura) - (5.677 * @idade)
   if @atividade == 1
     calorias_diarias = metabolismo_basal * 1.2
@@ -92,7 +129,5 @@ end
 
 entradas
 menu
-calculo
-
-#Fazer tratamentos de erros se usuário digitar errado as
-# opções de peso, altura e níveis de atividade
+calculo_metabolismo
+#calculo_imc
